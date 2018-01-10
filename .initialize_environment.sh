957991ad1f8c7d20c96685dea5658bc86a604107
gpwclark <gpwclark@gmail.com>
2018-01-10 03:40:53 +0000

    new functions in zsh and files for initializing environment

--- .initialize_environment.sh
+++ .initialize_environment.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 #install vim/git
+yaourt -S vim \
+        git
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+yaourt -S gradle
+git clone https://github.com/albertlatacz/java-repl ~/bin/java-repl.repo
+cd ~/bin/java-repl.repo && gradle shadowJar
