181ce60d405ce54e2394823000ff9b8be9828de4
price <price@eucleo.com>
2020-04-16 00:43:50 -0400

    edit init files to be x platform debian/arch forever

---
+++ .initialize_arch.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#install vim/git
+yay -Syu  && yay --noconfirm -S \
+        vim `#text editor` \
+        emacs `#ONLY for clj/cljs development` \
+        git `#scm` \
+        gradle `#build tool` \
+        maven  `#build tool` \
+        sl `#get on the train.` \
+        wget `# simple http get` \
+        slim `# login manager` \
+        i3-wm `#window manager` \
+        rofi `# dmenu replacement` \
+        i3blocks `#i3 status bar` \
+        xscreensaver `#screensaver` \
+        wxcam `#webcam` \
+        baobab `#visualize disk space` \
+        arandr `# manage screens` \
+        ifplugd `#handle connecting to wired interfaces` \
+        xorg-xev `#check key combinations` \
+        fail2ban `# keep out the bad guys` \
+        xorg-server `# should work in conjunction with slim and i3wm seamlessly` \
+        konsole `# preferred shell` \
+        firefox `#browser` \
+        chromium `# browsers` \
+        youtube-dl `#need a screensaver :)` \
+        tmux `#terminal multiplexer` \
+        mplayer `# all things media` \
+        leiningen `# clojure build tool` \
+        feh `#light weight previewer` \
+        pulseaudio `# sound` \
+        ponymix `# pulse sound cli  frontend` \
+        openvpn `# vpn` \
+        tree `# to visualize directory trees` \
+        xclip `# access paste bugger on cli` \
+        xorg-xbacklight `# backlight cli frontend used by i3blocks` \
+        acpi `# used for getting battery charge in i3blocks` \
+        xdotool `# for i3blocks` \
+        xorg-xprop `# for i3blocks` \
+        light-git `# brightness ctrl for gitj` \
+        eclim `# for vim java` \
+        gnome-calculator `#for calculations`
+        graphviz `# graph visualization/generations software` \
+        strace `# important for tracing system calls in linux`\
+        rlwrap `# important for wrapping cli programs`\
+        thunar `# windowed file browser` \
+        ranger `# cli file browser` \
+        burpsuite `# proxy to intercept things.` \
+        simplescreenrecorder `# screen recorder` \
+        pavucontrol `# for volume control` \
+        fzf `# ctrl-r on crack` \
+        scrot `# simple screenshot utility for X` \
+        lsd `# better ls` \
+        ipcalc `# subnet calcuclator` \
+        aspell `# check spelling on the CLI` \
+        aspell-en `# spelling dictionary for english`
---
+++ .initialize_debian.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+	xbase-clients \
+	git \
+	myrepos \
+	vcsh \
+	tmux \
+	vim \
+	x11-xserver-utils
